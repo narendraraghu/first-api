8f9687980d58b9f53a13003e8f68ca00345a488a
narendra <narendrakareli@yahoo.com>
2021-11-06 10:14:58 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask_restful import Resource, Api
+from flask import jsonify, request, Flask
+from flaskext.mysql import MySQL
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Kareli@123'
+app.config['MYSQL_DATABASE_DB'] = 'flaskapi'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+mysql.init_app(app)
+
+
+@app.route("/message/<string:message_string>", methods=["GET"])
+def message(message_string):
+    """Function to set message of a specific user in the MSQL database"""
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO message(message) " "VALUES(%s)", message_string)
+        row = cursor.fetchone()
+        conn.commit()
+        cursor.close()
+        conn.close()
+        resp = jsonify("Message created successfully!", row)
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
+    else:
+        return jsonify("Please provide valid message")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
